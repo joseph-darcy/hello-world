b5fd9d225b5eced35703768c4d82d76477f6aef5
joseph-darcy <joseph.darcy@maine.edu>
2016-02-16 20:36:19 -0500

    Displays the date to the user.
    Closes #2

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,8 +1,10 @@
+import java.util.*;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Date date = new Date();
+		System.out.println("Today's date is: " + date);
 
 	}
 
