848f6fbc0a87722038db3802d3a6b529974ea088
joseph-darcy <joseph.darcy@maine.edu>
2016-02-16 20:43:07 -0500

    Displays free memory to the user
    Closes #3

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Date date = new Date();
 		System.out.println("Today's date is: " + date);
+		System.out.println("Free Memory in bytes: " + Runtime.getRuntime().freeMemory());
+		
 
 	}
 
